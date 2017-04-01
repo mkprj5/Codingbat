4288bc30cd53d7fe7d24e6d08b41de0755fed46e
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 14:20:40 -0500

    Create sumHeights.java
    We have an array of heights, representing the altitude along a walking trail. Given start/end indexes into the array, return the sum of the changes for a walk beginning at the start index and ending at the end index. For example, with the heights {5, 3, 6, 7, 2} and start=2, end=4 yields a sum of 1 + 5 = 6. The start end end index will both be valid indexes into the array with start <= end.
    
    sumHeights([5, 3, 6, 7, 2], 2, 4) → 6
    sumHeights([5, 3, 6, 7, 2], 0, 1) → 2
    sumHeights([5, 3, 6, 7, 2], 0, 4) → 11

---
+++ Array-3/AP-1/sumHeights.java
@@ -0,0 +1,14 @@
+/*
+We have an array of heights, representing the altitude along a walking trail. Given start/end indexes into the array, return the sum of the changes for a walk beginning at the start index and ending at the end index. For example, with the heights {5, 3, 6, 7, 2} and start=2, end=4 yields a sum of 1 + 5 = 6. The start end end index will both be valid indexes into the array with start <= end.
+
+sumHeights([5, 3, 6, 7, 2], 2, 4) → 6
+sumHeights([5, 3, 6, 7, 2], 0, 1) → 2
+sumHeights([5, 3, 6, 7, 2], 0, 4) → 11
+*/
+public int sumHeights(int[] heights, int start, int end) {
+  int sum = 0;
+  for(int i=start; i < end; i++) {
+    sum = sum+Math.abs(heights[i+1] - heights[i]);
+  }
+  return sum;
+}
