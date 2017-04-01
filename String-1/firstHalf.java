5800349ea1ab14deb857833c22f38bd4f4b1ccf2
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 02:17:19 -0500

    Create firstHalf.java
    Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
    
    firstHalf("WooHoo") → "Woo"
    firstHalf("HelloThere") → "Hello"
    firstHalf("abcdef") → "abc"

---
+++ String-1/firstHalf.java
@@ -0,0 +1,10 @@
+/*
+Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+firstHalf("WooHoo") → "Woo"
+firstHalf("HelloThere") → "Hello"
+firstHalf("abcdef") → "abc"
+*/
+public String firstHalf(String str) {
+  return str.substring(0,str.length()/2);
+}
