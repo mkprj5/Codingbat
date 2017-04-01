707416d18ee95427422fd23928359f139051376a
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 02:13:56 -0500

    Create makeTags.java
    The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
    
    makeTags("i", "Yay") → "<i>Yay</i>"
    makeTags("i", "Hello") → "<i>Hello</i>"
    makeTags("cite", "Yay") → "<cite>Yay</cite>"

---
+++ String-1/makeTags.java
@@ -0,0 +1,10 @@
+/*
+The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+
+makeTags("i", "Yay") → "<i>Yay</i>"
+makeTags("i", "Hello") → "<i>Hello</i>"
+makeTags("cite", "Yay") → "<cite>Yay</cite>"
+*/
+public String makeTags(String tag, String word) {
+  return "<"+tag+">" +word+"</" +tag +">";
+}
