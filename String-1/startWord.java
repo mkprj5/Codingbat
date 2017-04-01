033a72b73797eec1fd3e16538ba96fa1b08f5cbf
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 02:43:30 -0500

    Update startWord.java

--- String-1/startWord.java
+++ String-1/startWord.java
@@ -6,5 +6,9 @@ startWord("hippo", "xip") → "hip"
 startWord("hippo", "i") → "h"
 */
 public String startWord(String str, String word) {
-  return str;
+       if (word.length() > str.length()) return "";
+        if (str.substring(0, word.length()).equals(word)) return word;
+        if (str.substring(1, word.length()).equals(word.substring(1)))
+            return str.charAt(0) + word.substring(1);
+        return "";
 }
