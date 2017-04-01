cc0b171d498ee3aaff908638d180d0792e601016
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 01:51:10 -0500

    Create icyHot.java

---
+++ Warmup-1/icyHot.java
@@ -0,0 +1,13 @@
+/*
+Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+
+icyHot(120, -1) → true
+icyHot(-1, 120) → true
+icyHot(2, 120) → false
+*/
+public boolean icyHot(int temp1, int temp2) {
+  if(temp1 < 0 && temp2 > 100 || temp1 > 100 && temp2 < 0) {
+    return true;
+  }
+  return false;
+}
