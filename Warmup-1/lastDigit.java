74837b41feed2b24c7fbdb07fb3a75eab2ee92c4
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 01:58:03 -0500

    Create lastDigit.java

---
+++ Warmup-1/lastDigit.java
@@ -0,0 +1,11 @@
+/*
+Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+
+lastDigit(7, 17) → true
+lastDigit(6, 17) → false
+lastDigit(3, 113) → true
+*/
+public boolean lastDigit(int a, int b) {
+  
+  return ( a%10 == b%10 );
+}
