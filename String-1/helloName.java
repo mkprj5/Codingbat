a5b80a4a518d51c044191de9ee530a9eb7426d59
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 02:12:48 -0500

    Create helloName.java
    Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
    
    helloName("Bob") → "Hello Bob!"
    helloName("Alice") → "Hello Alice!"
    helloName("X") → "Hello X!"

---
+++ String-1/helloName.java
@@ -0,0 +1,10 @@
+/*
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+helloName("Bob") → "Hello Bob!"
+helloName("Alice") → "Hello Alice!"
+helloName("X") → "Hello X!"
+*/
+public String helloName(String name) {
+  return "Hello " + name + "!";
+}
