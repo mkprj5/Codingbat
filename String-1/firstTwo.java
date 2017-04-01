d7a16151bb9c94f7c37c6ef26c6b58cd081c122f
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 02:16:40 -0500

    Create firstTwo.java
    Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". Note that str.length() returns the length of a string.
    
    firstTwo("Hello") → "He"
    firstTwo("abcdefg") → "ab"
    firstTwo("ab") → "ab"

---
+++ String-1/firstTwo.java
@@ -0,0 +1,13 @@
+/*
+Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". Note that str.length() returns the length of a string.
+
+firstTwo("Hello") → "He"
+firstTwo("abcdefg") → "ab"
+firstTwo("ab") → "ab"
+*/
+public String firstTwo(String str) {
+  if(str.length() > 2) {
+  str =str.substring(0,2);
+  }
+  return str;
+}
