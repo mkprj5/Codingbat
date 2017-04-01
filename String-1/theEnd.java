b4bff20213a6c0052923d1e3609c1b96c5a6d0e0
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 02:22:07 -0500

    Create theEnd.java
    Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
    
    theEnd("Hello", true) → "H"
    theEnd("Hello", false) → "o"
    theEnd("oh", true) → "o"

---
+++ String-1/theEnd.java
@@ -0,0 +1,14 @@
+/*
+Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+
+theEnd("Hello", true) → "H"
+theEnd("Hello", false) → "o"
+theEnd("oh", true) → "o"
+
+*/
+public String theEnd(String str, boolean front) {
+  if(front) {
+    return str.substring(0,1);
+  }
+  return str.substring(str.length()-1);
+}
