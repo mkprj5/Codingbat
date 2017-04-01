4254eae517bf9a2f47287fb26de5856f7ec8c6db
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 03:08:33 -0500

    Create start1.java
    Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
    
    start1([1, 2, 3], [1, 3]) → 2
    start1([7, 2, 3], [1]) → 1
    start1([1, 2], []) → 1

---
+++ Array-1/start1.java
@@ -0,0 +1,13 @@
+/*
+Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
+
+start1([1, 2, 3], [1, 3]) → 2
+start1([7, 2, 3], [1]) → 1
+start1([1, 2], []) → 1
+*/
+public int start1(int[] a, int[] b) {
+  int count = 0;
+  if(a.length > 0 && a[0] == 1) count++;
+  if(b.length > 0 && b[0] == 1) count++;
+  return count;
+}
