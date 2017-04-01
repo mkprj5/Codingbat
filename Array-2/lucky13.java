af4cf93be4d1984d513dc794c057cda7668e6f9a
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 04:28:23 -0500

    Create lucky13.java
    Given an array of ints, return true if the array contains no 1's and no 3's.
    
    lucky13([0, 2, 4]) → true
    lucky13([1, 2, 3]) → false
    lucky13([1, 2, 4]) → false

---
+++ Array-2/lucky13.java
@@ -0,0 +1,12 @@
+/*
+Given an array of ints, return true if the array contains no 1's and no 3's.
+
+lucky13([0, 2, 4]) → true
+lucky13([1, 2, 3]) → false
+lucky13([1, 2, 4]) → false
+*/
+Given an array of ints, return true if the array contains no 1's and no 3's.
+
+lucky13([0, 2, 4]) → true
+lucky13([1, 2, 3]) → false
+lucky13([1, 2, 4]) → false
