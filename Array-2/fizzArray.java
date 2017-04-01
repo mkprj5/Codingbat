83285e6d0d422131f463cb17e22d59d9a98b2dc0
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 04:29:52 -0500

    Create fizzArray.java
    Given a number n, create and return a new int array of length n, containing the numbers 0, 1, 2, ... n-1. The given n may be 0, in which case just return a length 0 array. You do not need a separate if-statement for the length-0 case; the for-loop should naturally execute 0 times in that case, so it just works. The syntax to make a new int array is: new int[desired_length] 
    
    fizzArray(4) → [0, 1, 2, 3]
    fizzArray(1) → [0]
    fizzArray(10) → [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]

---
+++ Array-2/fizzArray.java
@@ -0,0 +1,14 @@
+/*
+Given a number n, create and return a new int array of length n, containing the numbers 0, 1, 2, ... n-1. The given n may be 0, in which case just return a length 0 array. You do not need a separate if-statement for the length-0 case; the for-loop should naturally execute 0 times in that case, so it just works. The syntax to make a new int array is: new int[desired_length] 
+
+fizzArray(4) → [0, 1, 2, 3]
+fizzArray(1) → [0]
+fizzArray(10) → [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+*/
+public int[] fizzArray(int n) {
+  int[] nums = new int[n];
+  for(int i=0; i<nums.length; i++) {
+    nums[i] = i;
+  }
+  return nums;
+}
