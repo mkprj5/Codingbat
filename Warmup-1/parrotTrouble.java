090e7d1e2dc8737e50fc04a16115ccb9bfdb3c07
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 01:40:13 -0500

    Create parrotTrouble.java

---
+++ Warmup-1/parrotTrouble.java
@@ -0,0 +1,13 @@
+/*
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+parrotTrouble(true, 6) → true
+parrotTrouble(true, 7) → false
+parrotTrouble(false, 6) → false
+*/
+public boolean parrotTrouble(boolean talking, int hour) {
+  if(talking && (hour < 7 || hour > 20)) {
+    return true;
+  }
+  return false;
+}
