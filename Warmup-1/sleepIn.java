7e6a79d48e6961bab33cb8830793d292e2f55c0a
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 01:33:21 -0500

    Create sleepIn.java

---
+++ Warmup-1/sleepIn.java
@@ -0,0 +1,13 @@
+/*
+The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+sleepIn(false, false) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true
+*/
+public boolean sleepIn(boolean weekday, boolean vacation) {
+  if(!weekday || vacation) {
+    return true;
+  }
+  return false;
+}
