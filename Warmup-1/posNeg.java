ac0ed99f0d92964c93aa630da257cac13968fca5
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 01:42:23 -0500

    Create posNeg.java

---
+++ Warmup-1/posNeg.java
@@ -0,0 +1,18 @@
+/*
+Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
+posNeg(1, -1, false) → true
+posNeg(-1, 1, false) → true
+posNeg(-4, -5, true) → true
+
+*/
+
+public boolean posNeg(int a, int b, boolean negative) {
+  if(negative && a < 0 && b < 0) {
+    return true;
+  }
+  if (!negative && ( (a<0 && b > 0) || (a>0 && b<0))) {
+    return true;
+  }
+  return false;
+}
