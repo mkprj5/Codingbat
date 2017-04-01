11eed67faef1b7e0ec60283adea2b292abd1e803
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 03:05:14 -0500

    Create middleWay.java
    Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
    
    middleWay([1, 2, 3], [4, 5, 6]) → [2, 5]
    middleWay([7, 7, 7], [3, 8, 0]) → [7, 8]
    middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]

---
+++ Array-1/middleWay.java
@@ -0,0 +1,11 @@
+/*
+Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+middleWay([1, 2, 3], [4, 5, 6]) → [2, 5]
+middleWay([7, 7, 7], [3, 8, 0]) → [7, 8]
+middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
+*/
+public int[] middleWay(int[] a, int[] b) {
+  int c[] = {a[1],b[1]};
+  return c;
+}
