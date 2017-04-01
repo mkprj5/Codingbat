9d9e3c23941a8fc97010f89c0c751d5bdc26c17e
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 01:35:06 -0500

    Update monkeyTrouble.java

--- Warmup-1/monkeyTrouble.java
+++ Warmup-1/monkeyTrouble.java
@@ -1,9 +1,15 @@
 /*
+We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+
+monkeyTrouble(true, true) → true
+monkeyTrouble(false, false) → true
+monkeyTrouble(true, false) → false
+
+
+*/
 public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
   if(( aSmile && bSmile ) || ( !aSmile && !bSmile )) {
     return true;
   }
   return false;
 }
-
-*/
