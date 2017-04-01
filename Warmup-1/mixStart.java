6f9970363920cb2d3a846cccac93dd9ddbcf6849
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 01:53:57 -0500

    Create mixStart.java

---
+++ Warmup-1/mixStart.java
@@ -0,0 +1,13 @@
+/*
+Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+mixStart("mix snacks") → true
+mixStart("pix snacks") → true
+mixStart("piz snacks") → false
+*/
+public boolean mixStart(String str) {
+  if(str.length() >= 3 && str.substring(1,3).equals("ix")) {
+    return true;
+  }
+  return false;
+}
