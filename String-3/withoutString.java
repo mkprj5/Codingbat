1a330be5f5ef369012eb81a92a5523d18efac285
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 04:02:07 -0500

    Create withoutString.java
    Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
    
    withoutString("Hello there", "llo") → "He there"
    withoutString("Hello there", "e") → "Hllo thr"
    withoutString("Hello there", "x") → "Hello there"

---
+++ String-3/withoutString.java
@@ -0,0 +1,11 @@
+/*
+Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+
+withoutString("Hello there", "llo") → "He there"
+withoutString("Hello there", "e") → "Hllo thr"
+withoutString("Hello there", "x") → "Hello there"
+*/
+
+public String withoutString(String base, String remove) {
+  return base.replaceAll("(?i)" + remove , "");
+}
