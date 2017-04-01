04588c4645eaa311d69da33ce559f825f8934102
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 03:03:37 -0500

    Create reverse3.java
    Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
    
    reverse3([1, 2, 3]) → [3, 2, 1]
    reverse3([5, 11, 9]) → [9, 11, 5]
    reverse3([7, 0, 0]) → [0, 0, 7]

---
+++ Array-1/reverse3.java
@@ -0,0 +1,14 @@
+/*
+Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+
+reverse3([1, 2, 3]) → [3, 2, 1]
+reverse3([5, 11, 9]) → [9, 11, 5]
+reverse3([7, 0, 0]) → [0, 0, 7]
+*/
+public int[] reverse3(int[] nums) {
+  int num[] = new int[nums.length];
+  for(int i=0, j = nums.length-1; i < nums.length; i++, j--) {
+    num[i] = nums[j];
+  }
+  return num;
+}
