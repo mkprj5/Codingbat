4243a7ac4e30959e03109208235c788a87423b36
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 01:52:13 -0500

    Create hasTeen.java

---
+++ Warmup-1/hasTeen.java
@@ -0,0 +1,13 @@
+/*
+We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values, return true if 1 or more of them are teen.
+
+hasTeen(13, 20, 10) → true
+hasTeen(20, 19, 10) → true
+hasTeen(20, 10, 13) → true
+*/
+public boolean hasTeen(int a, int b, int c) {
+  if ( a >= 13 && a <=19 || b >= 13 && b <= 19 || c >= 13 && c <=19 ) {
+    return true;
+  }
+  return false;
+}
