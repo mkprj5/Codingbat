c8c1288fa5115b5a2101027d042ffff31454787a
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 03:47:28 -0500

    Create luckySum.java
    Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
    
    luckySum(1, 2, 3) → 6
    luckySum(1, 2, 13) → 3
    luckySum(1, 13, 3) → 1

---
+++ Logic-2/luckySum.java
@@ -0,0 +1,13 @@
+/*
+Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
+
+luckySum(1, 2, 3) → 6
+luckySum(1, 2, 13) → 3
+luckySum(1, 13, 3) → 1
+*/
+public int luckySum(int a, int b, int c) {
+  if( a == 13) { return 0; }
+  if( b== 13) return a;
+  if( c== 13) return a+b;
+  return a+b+c;
+}
