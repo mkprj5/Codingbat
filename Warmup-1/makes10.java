eb2a448c78bc256e717bce9d53143fa2f0cee9c9
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 01:40:57 -0500

    Create makes10.java

---
+++ Warmup-1/makes10.java
@@ -0,0 +1,15 @@
+/*
+Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+makes10(9, 10) → true
+makes10(9, 9) → false
+makes10(1, 9) → true
+
+*/
+
+public boolean makes10(int a, int b) {
+  if( (a == 10 || b ==10) || (a+b == 10) ) {
+    return true;
+  }
+  return false;
+}
