e19534fd1d35f25cc4978ac2ae1a2601f56ce4a6
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 03:10:05 -0500

    Create plusTwo.java
    Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
    
    plusTwo([1, 2], [3, 4]) → [1, 2, 3, 4]
    plusTwo([4, 4], [2, 2]) → [4, 4, 2, 2]
    plusTwo([9, 2], [3, 4]) → [9, 2, 3, 4]

---
+++ Array-1/plusTwo.java
@@ -0,0 +1,11 @@
+/*
+Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+
+plusTwo([1, 2], [3, 4]) → [1, 2, 3, 4]
+plusTwo([4, 4], [2, 2]) → [4, 4, 2, 2]
+plusTwo([9, 2], [3, 4]) → [9, 2, 3, 4]
+*/
+public int[] plusTwo(int[] a, int[] b) {
+  int c[] = {a[0],a[1],b[0],b[1]};
+  return c;
+}
