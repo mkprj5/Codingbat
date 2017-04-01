2aea70528a7784b56ba143ae512f28fd03ab3304
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 01:55:13 -0500

    Create intMax.java

---
+++ Warmup-1/intMax.java
@@ -0,0 +1,20 @@
+/*
+Given three int values, a b c, return the largest.
+
+intMax(1, 2, 3) → 3
+intMax(1, 3, 2) → 3
+intMax(3, 2, 1) → 3
+*/
+public int intMax(int a, int b, int c) {
+ int max;
+ if (a > b) {
+   max =a;
+ }
+ else {
+   max =b;
+ }
+ if (c > max) {
+   max =c;
+ }
+  return max;
+}
