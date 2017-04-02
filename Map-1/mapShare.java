ef6fcaa0312b05b797b8000a32bf1e875b3853b0
Mehul Prajapati <mehalcse@gmail.com>
2017-04-01 22:10:02 -0500

    Create mapShare.java
    Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
    
    mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
    mapShare({"b": "xyz", "c": "ccc"}) → {"b": "xyz"}
    mapShare({"a": "aaa", "c": "meh", "d": "hi"}) → {"a": "aaa", "b": "aaa", "d": "hi"}

---
+++ Map-1/mapShare.java
@@ -0,0 +1,14 @@
+/*
+Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
+
+mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
+mapShare({"b": "xyz", "c": "ccc"}) → {"b": "xyz"}
+mapShare({"a": "aaa", "c": "meh", "d": "hi"}) → {"a": "aaa", "b": "aaa", "d": "hi"}
+*/
+public Map<String, String> mapShare(Map<String, String> map) {
+  map.remove("c");
+  if(map.containsKey("a")) {
+    map.put("b", map.get("a"));
+  }
+  return map;
+}
